3adec953b1a336367348c7a6f1ce1c3bbf3f21eb
Oganesyan Levon <lev244@gmail.com>
2020-03-27 00:39:09 +0300

    27.03

--- schemedrawing/Graphs.Designer.cs
+++ schemedrawing/Graphs.Designer.cs
@@ -29,11 +29,24 @@
         private void InitializeComponent()
         {
             System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
             System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.label1 = new System.Windows.Forms.Label();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart3 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -49,24 +62,84 @@
             // 
             chartArea1.Name = "ChartArea1";
             this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            legend1.Title = "M=4";
+            this.chart1.Legends.Add(legend1);
             this.chart1.Location = new System.Drawing.Point(40, 165);
             this.chart1.Name = "chart1";
             series1.ChartArea = "ChartArea1";
             series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
-            series1.IsVisibleInLegend = false;
+            series1.Legend = "Legend1";
+            series1.LegendText = "Выделение разрыва";
             series1.Name = "Series1";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series2.Legend = "Legend1";
+            series2.LegendText = "Сковзной счёт";
+            series2.Name = "Series2";
             this.chart1.Series.Add(series1);
-            this.chart1.Size = new System.Drawing.Size(600, 600);
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(1069, 600);
             this.chart1.TabIndex = 1;
             this.chart1.Text = "chart1";
-            title1.Name = "r";
-            this.chart1.Titles.Add(title1);
+            // 
+            // chart2
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            legend2.Title = "M=6";
+            this.chart2.Legends.Add(legend2);
+            this.chart2.Location = new System.Drawing.Point(40, 782);
+            this.chart2.Name = "chart2";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series3.Legend = "Legend1";
+            series3.LegendText = "Выделение разрыва";
+            series3.Name = "Series1";
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series4.Legend = "Legend1";
+            series4.LegendText = "Сковзной счёт";
+            series4.Name = "Series2";
+            this.chart2.Series.Add(series3);
+            this.chart2.Series.Add(series4);
+            this.chart2.Size = new System.Drawing.Size(1069, 600);
+            this.chart2.TabIndex = 2;
+            this.chart2.Text = "chart2";
+            // 
+            // chart3
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.chart3.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            legend3.Title = "M=10";
+            this.chart3.Legends.Add(legend3);
+            this.chart3.Location = new System.Drawing.Point(1180, 594);
+            this.chart3.Name = "chart3";
+            series5.ChartArea = "ChartArea1";
+            series5.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series5.Legend = "Legend1";
+            series5.LegendText = "Выделение разрыва";
+            series5.Name = "Series1";
+            series6.ChartArea = "ChartArea1";
+            series6.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series6.Legend = "Legend1";
+            series6.LegendText = "Сковзной счёт";
+            series6.Name = "Series2";
+            this.chart3.Series.Add(series5);
+            this.chart3.Series.Add(series6);
+            this.chart3.Size = new System.Drawing.Size(1069, 600);
+            this.chart3.TabIndex = 3;
+            this.chart3.Text = "chart3";
             // 
             // Graphs
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(2474, 1429);
+            this.Controls.Add(this.chart3);
+            this.Controls.Add(this.chart2);
             this.Controls.Add(this.chart1);
             this.Controls.Add(this.label1);
             this.Name = "Graphs";
@@ -74,6 +147,8 @@
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.Graphs_Load);
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -83,5 +158,7 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart3;
     }
 }
--- schemedrawing/Graphs.cs
+++ schemedrawing/Graphs.cs
@@ -25,7 +25,7 @@ namespace schemedrawing
                 x = x + 0.001;
             }*/
             
-            chart1.Series[0].Points.AddXY(0.00001,0.70);
+            chart1.Series[1].Points.AddXY(0.00001,0.72);
             /*
             chart1.Series[0].Points.AddXY(0.1, 0.70);
             chart1.Series[0].Points.AddXY(0.2, 0.69);
@@ -40,12 +40,34 @@ namespace schemedrawing
             chart1.Series[0].Points.AddXY(1, 0.25);
             chart1.Series[0].Points.AddXY(1.5, 0.05);
             */
-            chart1.Series[0].Points.AddXY(0, 0.72);
-            chart1.Series[0].Points.AddXY(0.5, 0.58);
-            chart1.Series[0].Points.AddXY(1, 0.27);
-            chart1.Series[0].Points.AddXY(1.5, 0.05);
-            chart1.Series[0].Points.AddXY(2, 0.01);
+            //chart1.Series[0].Points.AddXY(0, 0.72);
+            chart1.Series[1].Points.AddXY(0.5, 0.58);
+            chart1.Series[1].Points.AddXY(1, 0.27);
+            chart1.Series[1].Points.AddXY(1.5, 0.05);
+            chart1.Series[1].Points.AddXY(2, 0.01);
+            
+           // chart1.Series[2].Points.AddXY(1.5, 0.05);
 
+
+
+
+            chart1.Series[0].Points.AddXY(0, 0.74);
+            chart1.Series[0].Points.AddXY(0.22, 0.68);
+            chart1.Series[0].Points.AddXY(0.45, 0.56);
+           // chart1.Series[1].Points.AddXY(0.5, 0.55);
+            //chart1.Series[1].Points.AddXY(0.55, 0.53);
+           // chart1.Series[1].Points.AddXY(0.6, 0.50);
+           // chart1.Series[1].Points.AddXY(0.65, 0.45);
+            chart1.Series[0].Points.AddXY(0.75, 0.40);
+            chart1.Series[0].Points.AddXY(0.8, 0.35);
+            chart1.Series[0].Points.AddXY(0.85, 0.32);
+            chart1.Series[0].Points.AddXY(0.9, 0.28);
+            chart1.Series[0].Points.AddXY(0.95, 0.25);
+            chart1.Series[0].Points.AddXY(1, 0.22);
+            chart1.Series[0].Points.AddXY(1.05, 0.2);
+            chart1.Series[0].Points.AddXY(1.3, 0.1);
+            chart1.Series[0].Points.AddXY(1.5, 0.05);
+            chart1.Series[0].Points.AddXY(2, 0.005);
 
 
             /*   chart2.Series[0].Points.AddXY(0, 0.66);
@@ -60,6 +82,45 @@ namespace schemedrawing
                chart2.Series[0].Points.AddXY(1, 0.35);
                chart2.Series[0].Points.AddXY(1.5, 0.22);
                */
+
+            chart2.Series[1].Points.AddXY(0, 0.72);
+            chart2.Series[1].Points.AddXY(0.33, 0.6);
+            chart2.Series[1].Points.AddXY(0.95, 0.2);
+            chart2.Series[1].Points.AddXY(1.45, 0.09);
+            chart2.Series[1].Points.AddXY(1.8, 0.05);
+            chart2.Series[1].Points.AddXY(2, 0.01);
+            //chart2.Series[1].Points.AddXY(2, 1.0);
+            chart2.Series[0].Points.AddXY(0, 0.71);
+            chart2.Series[0].Points.AddXY(0.24, 0.60);
+            chart2.Series[0].Points.AddXY(0.32, 0.55);
+            chart2.Series[0].Points.AddXY(0.41, 0.50);
+            // chart3.Series[1].Points.AddXY(0.8, 0.20);
+            chart2.Series[0].Points.AddXY(0.9, 0.2);
+            chart2.Series[0].Points.AddXY(1.5, 0.05);
+
+            chart2.Series[0].Points.AddXY(2, 0.01);
+
+
+            chart3.Series[0].Points.AddXY(0, 0.72);
+            chart3.Series[0].Points.AddXY(0.33, 0.55);
+            chart3.Series[0].Points.AddXY(0.7, 0.25);
+            chart3.Series[0].Points.AddXY(1.3, 0.05);
+            chart3.Series[0].Points.AddXY(1.8, 0.02);
+            chart3.Series[0].Points.AddXY(2, 0.01);
+            chart3.Series[1].Points.AddXY(0, 0.73);
+            chart3.Series[1].Points.AddXY(0.28, 0.63);
+            chart3.Series[1].Points.AddXY(0.35, 0.59);
+            chart3.Series[1].Points.AddXY(0.4, 0.55);
+           // chart3.Series[1].Points.AddXY(0.8, 0.20);
+            chart3.Series[1].Points.AddXY(0.9, 0.2);
+            chart3.Series[1].Points.AddXY(1.5, 0.05);
+
+            chart3.Series[1].Points.AddXY(2, 0.01);
+
+
+
+
+
         }
 
         private void Graphs_Load(object sender, EventArgs e)
